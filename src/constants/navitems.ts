690b681ddbbda2dd0885bcf441511e3ce80949c9
VivySilva <vivysilvaaraujo2004@gmail.com>
2026-02-09 15:15:42 -0300

    fix: Correct application route paths

--- src/constants/navitems.ts
+++ src/constants/navitems.ts
@@ -3,9 +3,8 @@ import type { NavItem } from "../types/nav";
 export const navItems:NavItem[] = [
     { label: 'Inicio', href: '/' },
     { label: 'Institucional', href: '/institutional' },
-    { label: 'Boletins', href: '/boletins' },
+    { label: 'Boletins', href: '/Newsletter' },
     { label: 'Equipe', href: '/equipe' },
     { label: 'Perguntas', href: '/quests' },
-    { label: 'Login', href: '/login' },
-    { label: 'Settings', href: '/settings' },
+    { label: 'Login', href: '/login' }
 ]
